cb011dcb01089c3986fd16e7f428e83d9b431a4b
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-18 11:24:53 -0600

    Add test for generating random key

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,9 +1,16 @@
 require './test/test_helper'
+require 'mocha/minitest'
 
 class EncryptTest < Minitest::Test
 
   def test_it_exists
     encrypt = Encrypt.new
-    assert_instance_of Encrypt, encrypt 
+    assert_instance_of Encrypt, encrypt
+  end
+
+  def test_it_can_generate_key
+    encrypt = mock("Encrypt Object")
+    encrypt.stubs(:generate_key).returns("12345")
+    assert_equal "12345", encrypt.generate_key
   end
 end
