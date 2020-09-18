9995f103268200cfade9fabdfd8b833f1704a278
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-18 12:02:08 -0600

    Add key attribute and initialize method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,9 +1,17 @@
 class Encrypt
+  attr_reader :key
+  def initialize
+    @key = self.generate_key
+  end
+
   def generate_key
     numbers = [0,1,2,3,4,5,6,7,8,9]
     key = []
     5.times do (key << numbers.sample)
     end
     key.join
+  end
+
+  def set_key_shifts(key = @key)
   end
 end
