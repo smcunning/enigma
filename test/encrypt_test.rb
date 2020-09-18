790acb4449c2d58489e032155523131e53dbf010
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-18 11:52:09 -0600

    Add test to find key shifts

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -13,4 +13,11 @@ class EncryptTest < Minitest::Test
     encrypt.stubs(:generate_key).returns("12345")
     assert_equal "12345", encrypt.generate_key
   end
+
+  def test_it_can_set_key_shifts
+    encrypt = mock("encrypt Object")
+    encrypt.stubs(:generate_key).returns("12345")
+    key = encrypt.generate_key
+    assert_equal ({a: 12, b: 23, c: 34, d: 45}), encrypt.set_key_shifts(key)
+  end
 end
