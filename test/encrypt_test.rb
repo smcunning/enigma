1921dcf23326bac28ec11041a797c7e2cb5558d5
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-19 12:25:37 -0600

    Add assert to attributes test for characters attribute

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -13,6 +13,9 @@ class EncryptTest < Minitest::Test
     encrypt.stubs(:key).returns("12345")
     assert_equal "12345", encrypt.key
     assert_equal [:a, :b, :c, :d], encrypt.shift_categories
+
+    expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    assert_equal expected, encrypt.characters
   end
 
   def test_it_can_generate_key
