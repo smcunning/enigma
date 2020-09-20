22470cf36d979e33c17e6b2e7081267bb9f896ca
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-19 18:29:14 -0600

    Add test for shifted character sets

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -56,12 +56,23 @@ class EncryptTest < Minitest::Test
 
   def test_it_can_find_total_shifts
     encrypt = Encrypt.new("hello world", "12345", "120787")
-    assert_equal ({a: 20, b: 17, c: 14, d: 20}), encrypt.total_shifts("111111", "9639")
+    assert_equal ({a: 21, b: 29, c: 37, d: 54}), encrypt.total_shifts("12345", "9639")
   end
 
   def test_it_can_assign_shift_categories_to_message
     encrypt = Encrypt.new("hello world", "12345", "120787")
     expected = ({:a=>['h','o','r'], :b=>['e',' ','l'], :c=>['l','w','d'], :d=>['l', 'o']})
     assert_equal expected, encrypt.set_message_to_shift_categories(encrypt.message)
+  end
+
+  def test_it_can_create_shifted_character_sets
+    encrypt = Encrypt.new("hello world", "12345", "120787")
+    expected = {
+      :a =>{'a'=>'v', 'b'=>'w', 'c'=>'x', 'd'=>'y', 'e'=>'z', 'f'=>' ', 'g'=>'a', 'h'=>'b', 'i'=>'c', 'j'=>'d', 'k'=>'e', 'l'=>'f', 'm'=>'g', 'n'=>'h', 'o'=>'i', 'p'=>'j', 'q'=>'k', 'r'=>'l', 's'=>'m', 't'=>'n', 'u'=>'o', 'v'=>'p', 'w'=>'q', 'x'=>'r', 'y'=>'s', 'z'=>'t', ' '=>'u'},
+      :b =>{'a'=>'c', 'b'=>'d', 'c'=>'e', 'd'=>'f', 'e'=>'g', 'f'=>'h', 'g'=>'i', 'h'=>'j', 'i'=>'k', 'j'=>'l', 'k'=>'m', 'l'=>'n', 'm'=>'o', 'n'=>'p', 'o'=>'q', 'p'=>'r', 'q'=>'s', 'r'=>'t', 's'=>'u', 't'=>'v', 'u'=>'w', 'v'=>'x', 'w'=>'y', 'x'=>'z', 'y'=>' ', 'z'=>'a', ' '=>'b'},
+      :c =>{'a'=>'k', 'b'=>'l', 'c'=>'m', 'd'=>'n', 'e'=>'o', 'f'=>'p', 'g'=>'q', 'h'=>'r', 'i'=>'s', 'j'=>'t', 'k'=>'u', 'l'=>'v', 'm'=>'w', 'n'=>'x', 'o'=>'y', 'p'=>'z', 'q'=>' ', 'r'=>'a', 's'=>'b', 't'=>'c', 'u'=>'d', 'v'=>'e', 'w'=>'f', 'x'=>'g', 'y'=>'h', 'z'=>'i', ' '=>'j'},
+      :d =>{'a'=>'a', 'b'=>'b', 'c'=>'c', 'd'=>'d', 'e'=>'e', 'f'=>'f', 'g'=>'g', 'h'=>'h', 'i'=>'i', 'j'=>'j', 'k'=>'k', 'l'=>'l', 'm'=>'m', 'n'=>'n', 'o'=>'o', 'p'=>'p', 'q'=>'q', 'r'=>'r', 's'=>'s', 't'=>'t', 'u'=>'u', 'v'=>'v', 'w'=>'w', 'x'=>'x', 'y'=>'y', 'z'=>'z', ' '=>' '},
+    }
+    assert_equal expected, encrypt.shifted_character_sets(encrypt.key, "9639")
   end
 end
