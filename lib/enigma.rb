500fce1b231dbeae45f37d8af0c8310066a1f5bd
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-20 13:36:03 -0600

    Add encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,4 +3,16 @@ class Enigma
   def initialize
     @message = File.open('message.txt', "r")
   end
+
+  def encrypt(message, key, date)
+    encryptor = Encryptor.new(message, key, date)
+    
+    {
+      encryption: encryptor.encrypt_message,
+      key: encryptor.key,
+      date: encryptor.date
+    }
+  end
+
+
 end
