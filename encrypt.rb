791d17a133ade93e00668607c31a8bd60dee3aad
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-20 17:20:22 -0600

    Create encrypt runner file

---
+++ encrypt.rb
@@ -0,0 +1,14 @@
+require './lib/enigma'
+require './lib/encryptor'
+
+handle = File.open(ARGV[0], "r")
+message = handle.read
+handle.close
+
+enigma = Enigma.new(message)
+
+encryption = File.open(ARGV[1], "w")
+encryption.write(enigma.encrypt(message))
+encryption.close
+
+puts "Created 'encrypted.txt' with the key #{enigma.encryptor.key} and date #{enigma.encryptor.date}"
