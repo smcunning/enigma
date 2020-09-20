d9892a408c79745f2b478534eca00f1c1616f5af
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-20 14:19:08 -0600

    Add attributes and require module

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -1,2 +1,18 @@
+require 'date'
+require_relative '../modules/shiftable'
+
 class Decryptor
-end 
+  include Shiftable
+  attr_reader :cipher_text,
+              :key,
+              :date,
+              :shift_categories,
+              :characters
+  def initialize(ciphertext, key, date = self.todays_date)
+    @cipher_text = ciphertext
+    @key = key
+    @date = date
+    @shift_categories = [:a, :b, :c, :d]
+    @characters = ("a".."z").to_a << ' '
+  end
+end
