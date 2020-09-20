2c8d894c2dd3e960287348a526fc3ee8a5d46690
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-20 14:37:56 -0600

    Add method for shift_categories_to_message using cipher_text

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -15,4 +15,15 @@ class Decryptor
     @shift_categories = [:a, :b, :c, :d]
     @characters = ("a".."z").to_a << ' '
   end
+
+  def set_message_to_shift_categories
+    shift_to_chars = {}
+    split_message = cipher_text.downcase.split('')
+    split_message.each_with_index do |char, index|
+     category = index % @shift_categories.count
+     (shift_to_chars[@shift_categories[category]] ||= []) << char
+    end
+    shift_to_chars
+  end
+  
 end
