5a4fa2ab547d171ac996504bade11574fef0bfac
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-20 14:18:16 -0600

    Add test for attributes

--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -5,7 +5,17 @@ require 'mocha/minitest'
 class DecryptorTest < Minitest::Test
 
   def test_it_exists
-    decryptor = Decryptor.new
+    decryptor = Decryptor.new("keder ohulw", "02715", "040895")
     assert_instance_of Decryptor, decryptor
+  end
+
+  def test_it_has_attributes
+    decryptor = Decryptor.new("keder ohulw", "02715", "040895")
+    assert_equal "keder ohulw", decryptor.cipher_text
+    assert_equal "02715", decryptor.key
+    assert_equal "040895", decryptor.date
+    assert_equal [:a, :b, :c, :d], decryptor.shift_categories
+    expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    assert_equal expected, decryptor.characters
   end
 end
